c5d71756b8e43d369b622aa4e70a23e5b5eb1fe2
wuyi5151 <313840325@qq.com>
2017-07-07 15:39:53 +0800

    美团jQuery mobile

--- meituan/javascript/index.js
+++ meituan/javascript/index.js
@@ -10,22 +10,56 @@ $(function(){//文档就绪函数
         //this是原生对象
         //$(this)是jQuery对象,$(this)[0]可转换成原生对象,jQuery方法返回的还是一个jQuery对象
         //两者可以相互转换
-        $(this).addClass("active").siblings().removeClass("active");
+       /* $(this).addClass("active").siblings().removeClass("active");
         $guideUl.css({
            left: -$(this).index() * $guideUl.width()/2
-        });
+        });*/
+        swipeGuide($(this).index());
     });
     $guideUl.on("swipeleft",function(){
-        $navSpan.eq(1).addClass("active").siblings().removeClass("active");
+       /* $navSpan.eq(1).addClass("active").siblings().removeClass("active");
         $guideUl.css({
             left:-$guideUl.width()/2
-        });
+        });*/
+        swipeGuide(1);
     }).on("swiperight",function(){
-        $navSpan.eq(0).addClass("active").siblings().removeClass("active");
+       /* $navSpan.eq(0).addClass("active").siblings().removeClass("active");
         $guideUl.css({
             left:0
+        });*/
+        swipeGuide(0)
+    });
+
+    $("#go-top").on("tap",function(){
+        if($(window).scrollTop()<=1){
+            return false;
+        }
+        $("body").animate({
+            scrollTop: 0
+        },500);
+        var top=$(this).css("top");
+        $(this).animate({
+            top: 0
+        },500,function(){
+            $(this).css("top",top);
         });
     });
+    /*oTop.onclick = function(){
+        var timer = setInterval(function(){
+            var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+            window.scrollTo(0, scrollTop-=50);
+            if(scrollTop <= 0){
+            clearInterval(timer);
+             }
+        }, 100);
+     };*/
+
+    function swipeGuide(index){
+        $navSpan.eq(index).addClass("active").siblings().removeClass("active");
+        $guideUl.css({
+            left: -index * $guideUl.width() / 2
+        });
+    }
 });
 /*$(document).ready(function(){
  文档就绪函数
